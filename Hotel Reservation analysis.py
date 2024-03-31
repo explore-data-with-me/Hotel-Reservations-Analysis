0197bb1398e02b06f3ac86a122f00bcc946a36fa
Pariyal <165175537+explore-data-with-me@users.noreply.github.com>
2024-03-31 20:32:38 +0530

    Update Hotel Reservation analysis.py

--- Hotel Reservation analysis.py
+++ Hotel Reservation analysis.py
@@ -2,16 +2,6 @@
 # coding: utf-8
 
 # # importing libraries
-
-# In[ ]:
-
-
-
-
-
-# In[59]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,80 +9,22 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
-
-# # loading the datasets
-
-# In[125]:
-
-
+# # Loading the datasets
 df = pd.read_csv(r"F:\End_to_End Projects\python\datasets\hotel_bookings 2.csv")
 
-
-# # exploratory data analysis and data analysis 
-
-# In[126]:
-
+# # Exploratory data analysis and data analysis 
 
 df.head()
-
-
-# In[127]:
-
-
 df.shape
-
-
-# In[128]:
-
-
 df.columns
-
-
-# In[129]:
-
-
 df.info()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[130]:
-
-
-df.info()
-
-
-# In[131]:
-
 
 df['reservation_status_date'] = pd.to_datetime(df['reservation_status_date'], format='%d/%m/%Y')
 
-
-# In[132]:
-
-
 df.info()
 
-
-# In[133]:
-
-
 df.describe(include = 'object')
-#### to get statostical data pf string columns 
-
-
-# In[134]:
-
+#### to get statistical data pf string columns 
 
 ### to see these different types in these object or string datatype columns
 
@@ -101,102 +33,43 @@ for col in df.describe(include = 'object').columns:
     print(df[col].unique())
     print("-"*50)
 
-
-# In[135]:
-
-
 df['hotel'].unique()
-### you can use unique function wih any column seperately as well
-
-
-# In[136]:
-
+### You can use a unique function with any column separately as well
 
 df.isnull().sum()
 
-
-# In[137]:
-
-
-#### droping the columns which are not of any use and has null values as well
+#### dropping the columns which are not of any use and have null values as well
 df.drop(['company','agent'],axis = 1, inplace = True)
 
-
-# In[138]:
-
-
 df.isnull().sum()
 
-
-# In[139]:
-
-
-### now we will drop the rows which has null value in column
+### Now we will drop the rows that have null values in the column
 
 df.dropna(inplace = True)
 
-
-# In[140]:
-
-
 df.isnull().sum()
-
-
-# In[141]:
-
 
 df.describe()
 
 
-# ## now in above description you can see there are a lot of outliers like take children column it shows max children are 10 which is a very rare case so it is an outlier although we won't focus on it this at all because for this project we have assumed initially that we don't have any outliers
-# 
+# ## Now in the above description you can see there are a lot of outliers take the children column it shows max children is 10 which is a very rare case so it is an outlier although we won't focus on this at all because for this project we have assumed initially that we don't have any outliers
 
-# # but we will focus on outlier in the column adr(average daily rate)...there is too much gap between min and max... so we would have to remove it
-
-# In[142]:
-
+# # but we will focus on the outlier in the column ADR(average daily rate)...there is too much gap between min and max... so we would have to remove it
 
 df = df[df['adr']<5000]
 
-
-# In[143]:
-
-
 df.describe()
 
-
-# In[ ]:
-
-
-
-
-
-# # 2nd step :  performing analysis and visualizations
-
-# In[144]:
-
+# # 2nd step:  performing analysis and visualizations
 
 cancelled = df['is_canceled'].value_counts()
 
-
-# In[145]:
-
-
-cancelled
-
-
-# In[146]:
-
-
 cancelled_percent = df['is_canceled'].value_counts(normalize = True)
-cancelled_percent
+
 ### to get the percentage
 
 
-# # now we can see that cancelation rate is almost 38% which is very high and a issue we need to resolve
-
-# In[147]:
-
+# # Now we can see that the cancelation rate is almost 38% which is very high and an issue we need to resolve
 
 print(cancelled_percent)
 
@@ -205,17 +78,7 @@ plt.title('Reservation status count') # setting the title of the plot.
 plt.bar(['Not Canceled','Canceled'],df['is_canceled'].value_counts(), edgecolor = 'k', width = 0.7)
 plt.show()
 
-
-# In[ ]:
-
-
-
-
-
-# # now we will check which hotel has what cancellation rate
-
-# In[148]:
-
+# # Now we will check which hotel has what cancellation rate
 
 plt.figure(figsize =(8,4))
 ax1 = sns.countplot(x = 'hotel' , hue = 'is_canceled', data = df, palette = 'Blues')
@@ -225,64 +88,15 @@ plt.ylabel('number of reservations')
 plt.legend(['not canceled','canceled'])
 plt.show()
                     
-                    
-
-
-# In[112]:
-
-
-"""
-plt.figure(figsize =(8,4))
-ax1 = sns.countplot(x = 'hotel' , hue = 'is_canceled', data = df, palette = 'Blues')
-plt.title('Reservation status in different hotels' , size = 20)
-legend_labels,_ = ax1. get_legend_handle_labels()
-ax1.legend(bbox_to_anchor(1,1))
-plt.xlabel('hotel')
-plt.ylabel('number of reservations')
-plt.legend(['not canceled','canceled'])
-plt.show()
-
-
-
-how the above statement was shown in the video
-"""
-
-
-# In[149]:
-
 
 resort_hotel = df[df['hotel'] == 'Resort Hotel']
 resort_hotel['is_canceled'].value_counts(normalize= True)
 
-
-# In[150]:
-
-
 city_hotel = df[df['hotel'] == 'City Hotel']
 city_hotel['is_canceled'].value_counts(normalize= True)
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[151]:
-
-
 resort_hotel = resort_hotel.groupby('reservation_status_date')[['adr']].mean()
 city_hotel = city_hotel.groupby('reservation_status_date')[['adr']].mean()
-
-
-# In[152]:
-
 
 plt.figure(figsize = (20,8))
 plt.title('Average Daily Rates in City and Resort Hotel', fontsize = 30)
@@ -292,12 +106,10 @@ plt.legend(fontsize = 20)
 plt.show()
 
 
-# # now calculating who has higher rates most of the time
-
-# In[153]:
-
+# # Now calculating who has higher rates most of the time
 
 # Calculate average daily rates for resort and city hotels
+
 average_adr_resort = resort_hotel['adr'].mean()
 average_adr_city = city_hotel['adr'].mean()
 
@@ -311,10 +123,6 @@ else:
 
 print("Hotel with higher average daily rates most of the time:", higher_rate_hotel)
 
-
-# In[154]:
-
-
 # Calculate average daily rates for resort and city hotels
 average_adr = [average_adr_resort, average_adr_city]
 hotel_types = ['Resort Hotel', 'City Hotel']
@@ -327,17 +135,7 @@ plt.xlabel('Hotel Type')
 plt.ylabel('Average Daily Rate')
 plt.show()
 
-
-# In[ ]:
-
-
-
-
-
-# In[155]:
-
-
-# Calculate total average daily rate
+# Calculate the total average daily rate
 total_average_adr = average_adr_resort + average_adr_city
 
 # Calculate percentages
@@ -356,26 +154,9 @@ plt.legend()
 # Adding percentage labels on top of bars
 plt.text('Resort Hotel', percentage_resort + 1, f'{percentage_resort:.1f}%', ha='center', va='bottom', fontsize=10, color='black')
 plt.text('City Hotel', percentage_resort + percentage_city / 2 + 1, f'{percentage_city:.1f}%', ha='center', va='bottom', fontsize=10, color='black')
-
 plt.show()
 
-
-# In[ ]:
-
-
-
-
-
 # #  now lets find out which month has higher cancellation rates
-
-# In[ ]:
-
-
-
-
-
-# In[156]:
-
 
 df['month'] = df['reservation_status_date'].dt.month
 plt.figure(figsize = (16,8))
@@ -387,24 +168,11 @@ plt.legend(['not canceled', 'canceled'])
 plt.show()
 
 
-# # highest cancelation rate is in january month and lowest in august
-# # lets find the reasons for the above
+# # The highest cancelation rate is in January month and the lowest in August
+# # Let's find the reasons for the above
 
-# In[ ]:
-
-
-
-
-
-# In[121]:
-
-
-"""     now we are finding the average daily rates per month so that we can fimd out is the reson for more cancellation
-in month of january is because of rates in that month or not          """
-
-
-# In[157]:
-
+"""     now we are finding the average daily rates per month so that we can find out the reason for more cancellation
+in the month of January is because of rates in that month or not          """
 
 plt.figure(figsize = (15,8))
 plt.title('ADR per Month', fontsize = 30)
@@ -412,90 +180,35 @@ sns.barplot(x='month',y='adr',data =df[df['is_canceled'] == 1].groupby('month')[
 plt.show()
 
 
-# ## yeah we can see that the rates are highest in the month of january so it can be the potential reason behind the more cancellation rates in this month
-# ## but it is not the only factor because as we can see month of august does not have lowest prices  but it still has lowest cancelation rates
+# ## Yeah we can see that the rates are highest in the month of January so it can be the potential reason behind the higher cancellation rates in this month
+# ## but it is not the only factor because as we can see the month of August does not have the lowest prices  but it still has lowest cancelation rates
 
-# In[ ]:
-
-
-
-
-
-# # now lets find top 10 countries which has maximum cancellation rates
-
-# In[ ]:
-
-
-
-
-
-# In[158]:
-
+# # Now let's find the top 10 countries which have maximum cancellation rates
 
 cancelled_data = df[df['is_canceled'] == 1]  ### only taking cancelled rows by doing this
-top_10_country = cancelled_data['country'].value_counts()[:10]   #### now kind of grouping those rows by country and then taking only first 10 by writing this [:10]
+top_10_country = cancelled_data['country'].value_counts()[:10]   #### now kind of grouping those rows by country and then taking only the first 10 by writing this [:10]
 
 plt.figure(figsize = (8,8))
 plt.title('Top 10 Countries with Reservation Cancelled')
-plt.pie(top_10_country, autopct='%.2f', labels = top_10_country.index)  ## plotting pie then need to check this label function properly , where and how can be it used.
+plt.pie(top_10_country, autopct='%.2f', labels = top_10_country.index)  ## plotting pie then need to check this label function properly, where and how can be it used.
 plt.show()
 
 
-# ### so we can see most of the cancellation is being done in the portugal country.  so we suggest that , hotels there needs to increase their facilities , do maintainence regulary and provide some discount offers as well
+# ### so we can see most of the cancellations are being done in the Portuguese country.  so we suggest that hotels there need to increase their facilities, do maintenance regularly and provide some discount offers as well
 
-# In[ ]:
-
-
-
-
-
-# # now lets find out where the clients are coming from, online or offline travel agents or others
-
-# In[ ]:
-
-
-
-
-
-# In[159]:
-
+# # Now let's find out where the clients are coming from, online or offline travel agents or others
 
 df['market_segment'].value_counts()
 
-
-# In[160]:
-
-
 df['market_segment'].value_counts(normalize = True)
-
-
-# In[161]:
-
 
 cancelled_data['market_segment'].value_counts(normalize = True)
 
+# ## Now our original hypothesis that says more customers are from offline bookings proves to be wrong here and in cancellation too online segment takes the lead
 
-# ## now our original hypothesis that says more customers are from offline bookings prove to be wrong here and in cancellation too online segment takes the lead
+# ### Now the reason can be the hotels in reality do not match the photos on their websites and are of poor quality.
 
-# In[ ]:
-
-
-
-
-
-# ### now the reason can be the hotels in reality does not match their photos on their websites and are of poor quality.
-
-# In[ ]:
-
-
-
-
-
-# ## now lets check the prices of reservations which has been cancelled compared to the reservations which has not been cancelled 
-# 
-
-# In[163]:
-
+# ## Now let's check the prices of reservations which has been canceled compared to the reservations which have not been canceled 
 
 cancelled_df_adr = cancelled_data.groupby('reservation_status_date')[['adr']].mean()
 cancelled_df_adr.reset_index(inplace = True)
@@ -512,17 +225,9 @@ plt.plot(not_cancelled_df_adr['reservation_status_date'], not_cancelled_df_adr['
 plt.plot(cancelled_df_adr['reservation_status_date'], cancelled_df_adr['adr'], label = 'cancelled')
 plt.legend()
 
-
-# In[166]:
-
-
 cancelled_df_adr = cancelled_df_adr[(cancelled_df_adr['reservation_status_date']>'2016') & (cancelled_df_adr['reservation_status_date']>'2017-09')]
 
 not_cancelled_df_adr = not_cancelled_df_adr[(not_cancelled_df_adr['reservation_status_date']>'2016') & (not_cancelled_df_adr['reservation_status_date']>'2017-09')]
-
-
-# In[167]:
-
 
 plt.figure(figsize=(20,6))
 plt.title('Average Daily Rate')
@@ -530,40 +235,4 @@ plt.plot(not_cancelled_df_adr['reservation_status_date'], not_cancelled_df_adr['
 plt.plot(cancelled_df_adr['reservation_status_date'], cancelled_df_adr['adr'], label = 'cancelled')
 plt.legend()
 
-
-# ## make the above chart acc to video
-
-# In[ ]:
-
-
-
-
-
-# ## cancelled rate is higher than the non cancelled rate .that means we can say price is directly proportional to cancellation rate. higher the price , higher the chances of cancellation
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[47]:
-
-
-for col in df.columns:
-    print(col)
-    print(df[col].unique())
-    print("-"*50)
-
-
-# In[ ]:
-
-
-
-
+# ## canceled rate is higher than the non-canceled rate That means we can say the price is directly proportional to the cancellation rate. higher the price , higher the chances of cancellation
